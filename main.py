6792338c500dcdcc4027694b2393175f14a98be0
Nicolas <nicolasmader001@gmail.com>
2026-03-10 09:41:58 +0100

    speichern und laden von speichern.txt hinzugefügt

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-#Zahlenratespiel
+# Zahlenratespiel
 import random
+
 spieler = None
+score = 0
+speichern = []
+
+
+# score und spieler speichern und laden
+def load_speichern():
+    with open("speichern.txt", "r") as file:
+        for save in file:
+            speichern.append(save)
+
+
+def save_speichern():
+    with open("speichern.txt", "w") as file:
+        for save in speichern:
+            file.write(save)
+
 
 # menü
 def menue():
     choice = None
 
-    while choice not in ["1","2","3"]:
+    while choice not in ["1", "2", "3"]:
         print("1 : Spieler Erstellen")
         print("2 : Spielen")
         print("3 : Beenden")
@@ -14,30 +31,43 @@ def menue():
         choice = input("Wähle eine Option:")
         print("Du hast gewählt:", choice)
 
-        if choice not in ["1","2","3"]:
+        if choice not in ["1", "2", "3"]:
             print("Ungültige Option, bitte erneut zwischen 1,2,3  wählen.")
 
     return choice
 
-#funktion aufrufen
-#zahl zufällig generieren
-def zahlgenerator():
 
-    zahlen_liste = ["1","2","3","4","5","6","7","8","9","10",
-                     "11","12","13","14","15","16","17","18","19","20","21"]
+# funktion aufrufen
+# zahl zufällig generieren
+def zahlgenerator():
+    zahlen_liste = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                    "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"]
 
     zufallzahl = random.choice(zahlen_liste)
     return int(zufallzahl)
 
 
 def abgleich(zufallzahl):
+    global score
+
     while True:
-        print("Bitte geben Sie eine Zahl von 1-21 ein.")
-        spielerzahl = int(input("Zahl eingeben:"))          #int: da nur ganze zahlen
+
+        try :
+            spielerzahl = int(input("Zahl eingeben:"))
+
+        except:
+            print("Bitte nur Zahlen!")
+            continue
+
+        if spielerzahl < 1 or spielerzahl > 21 :
+            print("Bitte geben Sie eine Zahl von 1-21 ein.")
+            continue
+
         print("Sie haben sich für", spielerzahl, "entschieden.")
 
         if spielerzahl == zufallzahl:
-            print("richtig score + 1")
+            score += 1
+            print("richtig score :", score)
             weiter = input("Weiter spielen (y/n):")
 
             if weiter == "y":
@@ -52,10 +82,16 @@ def abgleich(zufallzahl):
         elif spielerzahl > zufallzahl:
             print("Sie sind zu hoch die Zahl ist niedriger")
 
+
 def spieler_erstellen():
+    global score
     name = input("Wähle eine Namen:")
-    print("Du hast den Namen :",name,"gewählt")
+    print("Du hast den Namen :", name, "gewählt")
+    score = 0
     return name
+
+
+load_speichern()
 
 while True:
     wahl = menue()
@@ -71,11 +107,11 @@ while True:
             print("Namen erstellen")
 
         else:
-            print("Spieler:",spieler)
+            print("Spieler:", spieler)
             zufallzahl = zahlgenerator()
             abgleich(zufallzahl)
 
     elif wahl == "3":
         print("Spiel beendet")
+        save_speichern()
         break
-
